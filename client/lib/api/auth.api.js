b51ca1a62ca5975ec666ce1763e91fdd6460e1f7
vctorChamizo <victor.chamizo96@gmail.com>
2020-04-22 13:48:04 +0200

    Add server url token

--- client/lib/api/auth.api.js
+++ client/lib/api/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import _ from "lodash";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.SERVER_URL,
   withCredentials: true,
 });
 
