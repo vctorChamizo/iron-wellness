a5ec249fb564f0c4374c8b133fc092e209923ec0
vctorChamizo <victor.chamizo96@gmail.com>
2020-04-23 20:25:19 +0200

    Update enviroment tokens

--- server/lib/db-connection.js
+++ server/lib/db-connection.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 
 const connectionDB = async () => {
   try {
-    await mongoose.set("useCreateIndex", true).connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose
+      .set("useCreateIndex", true)
+      .connect(process.env.DB_URL_REMOTE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
     console.log(`Connection ready`);
   } catch (err) {
     console.error("Error connecting to mongo", err);
